962c74c77987fe2a9a8b9f06dfc1f6f43558d4d2
Gorbiel <gracz1212yt@gmail.com>
2023-03-05 17:27:17 +0100

    scaling and custom theme

--- befriend/src/befriend/app.py
+++ befriend/src/befriend/app.py
@@ -1,12 +1,17 @@
 import customtkinter
 
+customtkinter.set_default_color_theme("befriend_theme.json")
+customtkinter.set_appearance_mode("system")
+
+customtkinter.set_widget_scaling(0.5)  # widget dimensions and text size
+customtkinter.set_window_scaling(0.5)  # window geometry dimensions
 
 class BeFriend(customtkinter.CTk):
     def __init__(self):
         super().__init__()
 
         self.title("minimal example app")
-        self.minsize(400, 300)
+        self.minsize(720, 1280)
 
         self.button = customtkinter.CTkButton(master=self, command=self.button_callback)
         self.button.pack(padx=20, pady=20)
