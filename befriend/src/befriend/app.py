c36208aea6bd633d84b4378b7d06a3490e1fe82d
Rellikeht <michaelhemperek@gmail.com>
2023-03-05 17:48:11 +0100

    jajca

--- befriend/src/befriend/app.py
+++ befriend/src/befriend/app.py
@@ -11,6 +11,7 @@ else:
 customtkinter.set_widget_scaling(1)  # widget dimensions and text size
 customtkinter.set_window_scaling(0.5)  # window geometry dimensions
 
+
 class BeFriend(customtkinter.CTk):
     def __init__(self):
         super().__init__()
@@ -18,11 +19,13 @@ class BeFriend(customtkinter.CTk):
         self.title("minimal example app")
         self.minsize(720, 1280)
 
-        self.button = customtkinter.CTkButton(master=self, command=self.button_callback)
+        self.button = customtkinter.CTkButton(master=self,
+                                              command=self.button_callback)
         self.button.pack(padx=20, pady=20)
 
     def button_callback(self):
         print("button pressed")
+
 
 def main():
     return BeFriend()
