8d0143e9699af9bdb0a7218bc8e1920275551dea
polyoid <polyoid@miksiuk.com>
2023-03-06 04:19:36 +0100

    smol navbar changes uwu

--- befriend/src/befriend/messages.py
+++ befriend/src/befriend/messages.py
@@ -3,7 +3,15 @@ import yaml
 with open('resources/people.yml', 'r') as f: #wczytuje z yamla
     data = yaml.safe_load(f)
 
+class Person:
+    def __init__(self, name, isFriend):
+        self.name = name
+        self.isFriend = isFriend
 
 personlist = []
 for element in data:
-    
+    name = element
+    isFriend = data[element]["isFriend"]
+    personlist.append(Person(name, isFriend))
+
+
